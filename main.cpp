da2566d3ff9492019b49891ceb468ca3e387476d
Ted KIm <milkywood@gmail.com>
2025-04-11 14:13:39 +0900

    [feature] 38, implement 38page refactor

--- main.cpp
+++ main.cpp
@@ -5,16 +5,18 @@
 
 using std::vector;
 
-TEST(PrimeFactor, Of1) {
+class PrimeFixture : public ::testing::Test {
+public:
 	PrimeFactor pf;
+	vector<int> expected;
+};
 
-	vector<int> expected = {};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, pf.of(1));
 }
-TEST(PrimeFactor, Of2) {
-	PrimeFactor pf;
-
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, pf.of(2));
 }
 
